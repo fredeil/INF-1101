51539bf7fac750b37f2fc91727e3e25c42f4aa5e
fredeil <fredrik.eilertsen@gmail.com>
2018-01-15 21:14:17 +0100

    Add gitignore

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # INF-1101
-Datastructures and Algorithms in C
+Datastructures and Algorithms in C. 
+Probably some hacky and naive solutions, just because.
--- p1/arraylist.c
+++ p1/arraylist.c
@@ -1,9 +1,25 @@
 #include "list.h"
 #include "common.h"
+#include <assert.h>
 
+struct list 
+{
+    int size;
+    void *data;
+    cmpfunc_t comparer;
+};
 
-struct list;
-typedef struct list list_t;
+list_t *list_create(cmpfunc_t cmpfunc)
+{
+    list_t *list = malloc(sizeof(list_t));
+
+    assert(list != NULL);
+
+    list->size = 0;
+    list->comparer = cmpfunc;
+
+    return list;
+}
 
 void list_destroy(list_t *list)
 {
@@ -12,7 +28,8 @@ void list_destroy(list_t *list)
 
 int list_size(list_t *list)
 {
-    return 0;
+    assert(list != NULL);
+    return list->size;
 }
 
 void list_addfirst(list_t *list, void *elem)
@@ -45,7 +62,10 @@ void list_sort(list_t *list)
 
 }
 
-struct list_iter;
+struct list_iter 
+{
+ 
+};
 
 list_iter_t *list_createiter(list_t *list)
 {
@@ -65,4 +85,11 @@ int list_hasnext(list_iter_t *iter)
 void *list_next(list_iter_t *iter)
 {
 
+}
+
+int main(void) 
+{
+    list_t *list = list_create(NULL);
+    puts("mordi");
+    printf("Size: %d\n", list_size(list));
 }
