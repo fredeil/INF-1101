d490b35856f2df9ec22e0481f025ffdf28c1a000
mortenbertheussen <morten.bertheussen@gmail.com>
2018-01-23 20:37:30 +0100

    added print list, bubblesort next

--- Morten/Oblig/p1-pre/reversewords.c
+++ Morten/Oblig/p1-pre/reversewords.c
@@ -3,6 +3,22 @@
 #include <string.h>
 #include <stdio.h>
 
+void bubbleSort(list_t *list)
+{
+    list_iter_t *left = list_createiter(list);
+    list_iter_t *right = list_createiter(list);
+    list_next(right);
+}
+
+void printList(list_t *list)
+{
+    list_iter_t *iter = list_createiter(list);
+
+    while(list_hasnext(iter) == 1)
+    {
+        printf("%s\n", list_next(iter));
+    }
+} 
 
 int stringComparer(void *str1, void *str2)
 {
@@ -16,14 +32,14 @@ int main(int argc, char **argv)
     void *ptr1 = "Morten";
     void *ptr2 = "Morten";
 
-    printf("Lenght: %d\n", list_size(list));
-    list_addfirst(list, ptr1);
-    list_addfirst(list, ptr2);
-    printf("Lenght: %d\n", list_size(list));
+    list_addfirst(list, "Morten");
+    list_addfirst(list, "fredrik");
+    list_addfirst(list, "Kenneth");
+    list_addfirst(list, "Alexander");
 
 
 
-    printf("%d\n", list_contains(list,"Morten"));
+    printList(list);
 
     return 0;
 }
