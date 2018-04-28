f8562a2260cc6a57f934e6bba12329ce0e008b06
fredeil <fredrik.eilertsen@gmail.com>
2018-04-28 13:26:51 +0200

    :tada:

--- INF1101-fei003-a2/src/index.c
+++ INF1101-fei003-a2/src/index.c
@@ -38,9 +38,7 @@ index_t *index_create()
 {
     index_t *index = calloc(1, sizeof(index_t));
     if (index == NULL)
-    {
         fatal_error("Out of memory");
-    }
 
     index->hashmap = map_create(compare_strings, hash_string);
 
@@ -52,20 +50,10 @@ void index_destroy(index_t *index)
 {
     map_destroy(index->hashmap, free, free);
     if (index->iterator != NULL)
-    {
         list_destroyiter(index->iterator);
-    }
 
     free(index);
 }
-
-
-
-/*
- * 
- * NOTE: It is the responsibility of index_addpath() to deallocate (free)
- *       'path' and the contents of the 'words' list.
- */
 
 // Adds the given path to the given index, and index the given list of words under that path
 void index_addpath(index_t *index, char *path, list_t *words)
@@ -118,9 +106,7 @@ void index_addpath(index_t *index, char *path, list_t *words)
             // Insert new word into hashmap
             doc_t *doc = calloc(1, sizeof(doc_t));
             if (doc == NULL)
-            {
                 fatal_error("Out of memory");
-            }
 
             set_t *set = set_create(compare_docs);
 
@@ -148,9 +134,7 @@ list_t *index_query(index_t *index, list_t *query, char **errmsg)
 {
     index->iterator = list_createiter(query);
     if (list_hasnext(index->iterator))
-    {
         index->current_word = list_next(index->iterator);
-    }
 
     list_t *list = NULL;
     set_t *set = parse_query(index, errmsg);
@@ -164,9 +148,7 @@ list_t *index_query(index_t *index, list_t *query, char **errmsg)
         {
             query_result_t *query_result = malloc(sizeof(query_result_t));
             if(query_result == NULL)
-            {
                 fatal_error("Out of memory");
-            }
             
             doc_t *doc = set_next(iter);
             query_result->path = doc->path;
@@ -183,93 +165,93 @@ list_t *index_query(index_t *index, list_t *query, char **errmsg)
     return list;
 }
 
-// something
+// query ::= andterm | andterm "ANDNOT" query
 set_t *parse_query(index_t *index, char **errmsg)
 {
-    set_t *terms = parse_andterm(index, errmsg);
+    // andterm
+    set_t *term = parse_andterm(index, errmsg);
+
+    // andterm "ANDNOT" query
     if (compare_strings(index->current_word, "ANDNOT") == 0)
     {
         if (list_hasnext(index->iterator))
         {
             index->current_word = list_next(index->iterator);
-            return set_difference(terms, parse_query(index, errmsg));
+            return set_difference(term, parse_query(index, errmsg));
         }
     }
 
-    return terms;
+    return term;
 }
 
-// something
+// andterm ::= orterm | orterm "AND" andterm
 set_t *parse_andterm(index_t *index, char **errmsg)
 {
-    set_t *terms = parse_orterm(index, errmsg);
+    // orterm
+    set_t *term = parse_orterm(index, errmsg);
 
+    // orterm "AND" andterm
     if (compare_strings(index->current_word, "AND") == 0)
     {
         if (list_hasnext(index->iterator))
         {
             index->current_word = list_next(index->iterator);
-            return set_intersection(terms, parse_andterm(index, errmsg));
+            return set_intersection(term, parse_andterm(index, errmsg));
         }
     }
 
-    return terms;
+    return term;
 }
 
-// Something
+// orterm ::= term | term "OR" orterm
 set_t *parse_orterm(index_t *index, char **errmsg)
 {
-    set_t *terms = parse_term(index, errmsg);
+    // term
+    set_t *term = parse_term(index, errmsg);
+
+    // term "OR" orterm
     if (compare_strings(index->current_word, "OR") == 0)
     {
         if (list_hasnext(index->iterator))
         {
             index->current_word = list_next(index->iterator);
-            return set_union(terms, parse_orterm(index, errmsg));
+            return set_union(term, parse_orterm(index, errmsg));
         }
     }
 
-    return terms;
+    return term;
 }
 
+// term ::= "(" query ")" | <word>
 set_t *parse_term(index_t *index, char **errmsg)
 {
     set_t *set = NULL;
 
-    // If the query starts with an opening ()
+    // "(" query ")"
     if (compare_strings(index->current_word, "(") == 0)
     {
         if (list_hasnext(index->iterator))
-        {
             index->current_word = list_next(index->iterator);
-        }
 
         set = parse_query(index, errmsg);
 
         if (list_hasnext(index->iterator))
-        {
             index->current_word = list_next(index->iterator);
-        }
 
         if (compare_strings(index->current_word, ")") != 0)
-        {
             *errmsg = "Something went wrong..";
-        }
+
         else if (list_hasnext(index->iterator))
-        {
             index->current_word = list_next(index->iterator);
-        }
     }
+
+    // <word>
     if (map_haskey(index->hashmap, index->current_word) == 1)
     {
         // This happens if the query has no parenthesis
-
         set = map_get(index->hashmap, index->current_word);
-
         if (list_hasnext(index->iterator))
-        {
             index->current_word = list_next(index->iterator);
-        }
     }
 
     return set;
