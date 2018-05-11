dba654fe425dfc5d02d5c722f96599b3380e951d
fredeil <fredrik.eilertsen@gmail.com>
2018-05-11 19:14:18 +0200

    :memo:

--- sorting/quicksort.c
+++ sorting/quicksort.c
@@ -1,19 +1,12 @@
-/* author: Marius Andreassen (marius.andreassen@uit.no) */
-
-// Application files
 #include "sort.h"
 
-/* Select pivot and partition data */
 static int partition(int *data, int min, int max)
 {
-  /* Your code goes here */
-  return 0; // Return the pivot, (probably) not zero!
+  return 0;
 }
 
-/* Recursive quicksort algorithm */
 static void _quicksort(int *data, int min, int max)
 {
-  /* Your code goes here */
 }
 
 void quicksort(int *data, int size)
--- sorting/selection_sort.c
+++ sorting/selection_sort.c
@@ -1,7 +1,5 @@
-/* author: Marius Andreassen (marius.andreassen@uit.no) */
 #include "sort.h"
 
 void selection_sort(int *data, int size)
 {
-  /* Your code here */
 }
